9ea1e55a656021ab4e8ec2a17d42f17f45acd94a
Lisa Nguyen <lisanguyensc@gmail.com>
2022-09-13 23:35:26 +0000

    Completed local storage functionality along with new object

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem('Journal Entry Data');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('Journal Entry Data', dataJSON);
+});
